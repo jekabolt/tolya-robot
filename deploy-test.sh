c76617609017cd9adc68d8b6bf5f30bcc1ac7dd4
jekabolt <jekabolt@yahoo.com>
2020-11-29 22:00:01 +0300

    fix

--- deploy-test.sh
+++ deploy-test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-# chmod 600 .travis/id_rsa # Allow read access to the private key
-# ssh-add .travis/id_rsa # Add the private key to SSH
+ls -la .travis
+chmod 600 .travis/id_rsa # Allow read access to the private key
+ssh-add .travis/id_rsa # Add the private key to SSH
 
 ssh -tt -o StrictHostKeyChecking=no $REMOTE_USER@$SSH_KNOWNHOST <<EOF
 docker pull ${TRAVIS_REPO_SLUG}:${IMAGE_VERSION} 
