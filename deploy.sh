3f509a346b66af81702ef59f7b84620c6325db0c
jekabolt <jekabolt@yahoo.com>
2020-12-02 23:06:50 +0300

    rm insufficient

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,11 @@
-set -xe
+#!/bin/bash
 
-if [ $TRAVIS_BRANCH == 'master' ] ; then
-  eval "$(ssh-agent -s)"
-  ssh-add ~/.ssh/id_rsa
+eval "$(ssh-agent -s)" # Start ssh-agent cache
+ls -la 
+chmod 600 .travis/id_rsa # Allow read access to the private key
+ssh-add .travis/id_rsa # Add the private key to SSH
 
-  rsync -a --exclude={'/node_modules','/src','/public'} client/ travis@<droplet ipaddress>:/home/<sudo user>/demo/client
-  rsync -a server/ travis@<droplet ipaddress>:/home/<sudo user>/demo/server
-else
-  echo "Not deploying, since the branch isn't master."
-fi
-
-# https://www.codedisciples.in/travis-digitalocean.html
+ssh -tt -o StrictHostKeyChecking=no $REMOTE_USER@$SSH_KNOWNHOST <<EOF
+docker pull ${TRAVIS_REPO_SLUG}:${IMAGE_VERSION} 
+docker run -d ${TRAVIS_REPO_SLUG}:${IMAGE_VERSION}
+EOF
